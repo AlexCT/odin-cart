7f33042d7ceb5235fd98aa9da4deca3825bd9b26
AlexCT <tresseltac@gmail.com>
2022-07-02 23:05:11 -0700

    Add cart logic

--- src/Shop.js
+++ src/Shop.js
@@ -1,16 +1,24 @@
+import React, { useState } from "react";
 import Card from "./components/Card";
-import icon from "./images/icon.png"
-import hammer from "./images/hammer.png"
-import helmet from "./images/helmet.png"
-import shield from "./images/shield.png"
+import icon from "./images/icon.png";
+import hammer from "./images/hammer.png";
+import helmet from "./images/helmet.png";
+import shield from "./images/shield.png";
 
 const Shop = () => {
+
+    const [cart, setCart] = useState(0);
+
+    function updateCart(num) {
+        setCart(cart + num);
+    }
+
     return (
         <div>
-            <h1>Items in Cart: 0</h1>
-            <Card name="Hammer" qty="0" image={hammer}/>
-            <Card name="Helmet" qty="0" image={helmet}/>
-            <Card name="Shield" qty="0" image={shield}/>
+            <h1>Items in Cart: {cart}</h1>
+            <Card name="Hammer" qty="0" image={hammer} updateCart={updateCart}/>
+            <Card name="Helmet" qty="0" image={helmet} updateCart={updateCart}/>
+            <Card name="Shield" qty="0" image={shield} updateCart={updateCart}/>
         </div>
 
     );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
+import Shop from "../Shop"
 
 function Card(props) {
+
+    const [qty, setQty] = useState(0);
+
     return (
         <div id="card">
             <h1>{props.name}</h1>
             <img src={props.image} width="64" height="64"></img>
             <div id="qtybox">
-                <input type="number" id="quantity" name="quantity" value={props.qty}></input>
-                <input type="button" id="increase" name="increase" value="+"></input>
-                <input type="button" id="decrease" name="decrease" value ="-"></input>
+                <input type="number" id="quantity" name="quantity" value={qty}></input>
+                <input type="button" id="increase" name="increase" value="+" onClick={() => setQty(qty + 1)}></input>
+                <input type="button" id="decrease" name="decrease" value ="-" onClick={() => setQty(qty - 1)}></input>
             </div>
-            <input type="submit" id="addtocart" name="addtocart" value="Add to Cart"></input>
+            <input type="submit" id="addtocart" name="addtocart" value="Add to Cart" onClick={() => props.updateCart(qty)}></input>
         </div>
     );
 };
